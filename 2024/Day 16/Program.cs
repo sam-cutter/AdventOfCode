5d653ced4c4cfac9c7e66cfb0e63f32b0e2ecd50
Sam Cutter <swcutter@pm.me>
2024-12-16 11:40:05 +0000

    Began solving Day 16

---
+++ 2024/Day 16/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace Day_16
+{
+    internal class Program
+    {
+        struct Position
+        {
+            public int top;
+            public int left;
+        }
+
+        struct Node
+        {
+            public int workingValue;
+            public bool locked;
+            public Facing facing;
+        }
+
+        enum Facing
+        {
+            North,
+            East,
+            South,
+            West
+        }
+
+        static void Main(string[] args)
+        {
+            string[] maze = File.ReadAllLines("test1.txt");
+            Dictionary<Position, Node> cache = new Dictionary<Position, Node>();
+
+            Position startingPosition = new Position();
+            Position endingPosition = new Position();
+
+            for (int top = 0; top < maze.Length; top++)
+            {
+                for (int left = 0; left < maze[top].Length; left++)
+                {
+                    if (maze[top][left] == 'S')
+                    {
+                        startingPosition.top = top;
+                        startingPosition.left = left;
+                    } else if (maze[top][left] == 'E')
+                    {
+                        endingPosition.top = top;
+                        endingPosition.left = left;
+                    }
+                }
+            }
+
+            Node startingNode = new Node() { workingValue = 0, locked = true, facing = Facing.East };
+
+            cache.Add(startingPosition, startingNode);
+
+            Djikstras(maze, cache, startingPosition, Facing.East, 0);
+
+            Console.WriteLine(cache[endingPosition].workingValue);
+
+            Console.ReadKey();
+        }
+
+        static void Djikstras(string[] maze, Dictionary<Position, Node> cache, Position position, Facing facing, int points)
+        {
+            Position positionInFront = new Position() { top = position.top, left = position.left };
+
+            switch (facing)
+            {
+                case Facing.North:
+                    positionInFront.top -= 1;
+                    break;
+                case Facing.South:
+                    positionInFront.top += 1;
+                    break;
+                case Facing.West:
+                    positionInFront.left -= 1;
+                    break;
+                case Facing.East:
+                    positionInFront.left += 1;
+                    break;
+            }
+
+            Position up;
+            up.left = position.left;
+            up.top = position.top - 1;
+
+            Position down;
+            down.left = position.left;
+            down.top = position.top + 1;
+
+            Position left;
+            left.left = position.left - 1;
+            left.top = position.top;
+
+            Position right;
+            right.left = position.left + 1;
+            right.top = position.top;
+
+            Position[] neighbours = {up, down, left, right};
+
+            List<Position> validNeighbours = new List<Position>();
+
+            foreach (Position neighbour in neighbours)
+            {
+                if (neighbour.top < 0 || neighbour.left < 0) continue;
+                if (neighbour.top >= maze.Length || neighbour.left >= maze[0].Length) continue;
+                if (maze[neighbour.top][neighbour.left] == '#') continue;
+                if (cache.ContainsKey(neighbour) && cache[neighbour].locked) continue;
+
+                if (neighbour.top == positionInFront.top && neighbour.left == positionInFront.left)
+                {
+                    int neighbourPoints = points + 1;
+
+                    if (cache.ContainsKey(neighbour) && cache[neighbour].workingValue < neighbourPoints) continue;
+
+                    cache[neighbour] = new Node() { workingValue = points + 1, locked = false, facing = facing };
+                }
+                else
+                {
+                    int neighbourPoints = points + 1000 + 1;
+
+                    if (cache.ContainsKey(neighbour) && cache[neighbour].workingValue < neighbourPoints) continue;
+
+                    Facing neighbourFacing = facing;
+
+                    if (neighbour.top < position.top) neighbourFacing = Facing.North;
+                    if (neighbour.top > position.top) neighbourFacing = Facing.South;
+                    if (neighbour.left < position.left) neighbourFacing = Facing.West;
+                    if (neighbour.left > position.left) neighbourFacing = Facing.East;
+
+                    cache[neighbour] = new Node() { workingValue = points + 1000 + 1, locked = false, facing = neighbourFacing };
+                }
+
+                validNeighbours.Add(neighbour);
+            }
+
+            List<KeyValuePair<Position, Node>> unlockedKVPs = cache.Where(kvp => !kvp.Value.locked).ToList();
+
+            if (unlockedKVPs.Count() > 0)
+            {
+                KeyValuePair<Position, Node> next = unlockedKVPs.OrderByDescending(kvp => kvp.Value.workingValue).First();
+                cache[next.Key] = new Node() { workingValue = next.Value.workingValue, locked = true, facing = next.Value.facing };
+
+                Djikstras(maze, cache, next.Key, next.Value.facing, next.Value.workingValue);
+            }
+
+            return;
+        }
+    }
+}
