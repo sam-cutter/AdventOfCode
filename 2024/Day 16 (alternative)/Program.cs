ecdea2cf5dfd7e80a186d26867f6ee7c988f8412
Sam Cutter <swcutter@pm.me>
2024-12-18 08:00:17 +0000

    Began re-attempting Day 16

---
+++ 2024/Day 16 (alternative)/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day_16__alternative_
+{
+    internal class Program
+    {
+        static int MAZE_HEIGHT = 0;
+        static int MAZE_WIDTH = 0;
+
+        struct Position
+        {
+            public int top, left;
+        }
+
+        struct Node
+        {
+            public Dictionary<Direction, (int points, bool locked)> lowestPoints;
+        }
+
+        enum Direction
+        {
+            North,
+            East,
+            South,
+            West
+        }
+
+        static void Main(string[] args)
+        {
+            string[] maze = File.ReadAllLines("input.txt");
+            Dictionary<Position, Node> cache = new Dictionary<Position, Node>();
+
+            MAZE_HEIGHT = maze.Length;
+            MAZE_WIDTH = maze[0].Length;
+
+            Position startingPosition = new Position();
+            Position endingPosition = new Position();
+
+            for (int top = 0; top < maze.Length; top++)
+            {
+                for (int left = 0; left < maze[top].Length; left++)
+                {
+                    if (maze[top][left] == 'S')
+                    {
+                        startingPosition.top = top;
+                        startingPosition.left = left;
+                    }
+                    else if (maze[top][left] == 'E')
+                    {
+                        endingPosition.top = top;
+                        endingPosition.left = left;
+                    }
+                }
+            }
+        }
+
+        static void UpdateNeighbours(Dictionary<Position, Node> cache, Position position)
+        {
+            Dictionary<Direction, Position> neighbours = ConstructNeighbours(position);
+        }
+
+        static Dictionary<Direction, Position> ConstructNeighbours(Position position)
+        {
+            Dictionary<Direction, Position> neighbours = new Dictionary<Direction, Position>
+            {
+                [Direction.North] = new Position() { top = position.top - 1, left = position.left },
+                [Direction.East] = new Position() { top = position.top, left = position.left + 1 },
+                [Direction.South] = new Position() { top = position.top + 1, left = position.left },
+                [Direction.West] = new Position() { top = position.top, left = position.left - 1 }
+            };
+
+            return (Dictionary<Direction, Position>)neighbours
+                .Where(neighbour => neighbour.Value.top >= 0 && neighbour.Value.left >= 0)
+                .Where(neighbour => neighbour.Value.top < MAZE_HEIGHT && neighbour.Value.left < MAZE_WIDTH);
+        }
+    }
+}
---
+++ 2024/Day 16 (alternative)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Day 16 (alternative)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day 16 (alternative)")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0270524f-27d4-4eaa-b65b-7d188952d311")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
